874ad481a589d1983d1a41ded74ce9589c97de31
rohan <negi.rohan@gmail.com>
2016-07-02 22:44:14 +0530

    db Updates

--- videoTutorials.sql
+++ videoTutorials.sql
@@ -28,7 +28,7 @@ USE tutorialsdb;
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `Name` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
@@ -37,27 +37,12 @@ CREATE TABLE `categories` (
 --
 
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` (`id`,`name`) VALUES 
+ (1,'Categoyr1'),
+ (2,'Category2'),
+ (3,'Category3'),
+ (4,'Category4');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-
-
---
--- Definition of table `course_category_r`
---
-
-DROP TABLE IF EXISTS `course_category_r`;
-CREATE TABLE `course_category_r` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `course_id` int(10) NOT NULL,
-  `category_id` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `course_category_r`
---
-
-/*!40000 ALTER TABLE `course_category_r` DISABLE KEYS */;
-/*!40000 ALTER TABLE `course_category_r` ENABLE KEYS */;
 
 
 --
@@ -70,7 +55,7 @@ CREATE TABLE `course_subscription` (
   `course_id` int(10) NOT NULL,
   `user_id` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `course_subscription`
@@ -78,7 +63,8 @@ CREATE TABLE `course_subscription` (
 
 /*!40000 ALTER TABLE `course_subscription` DISABLE KEYS */;
 INSERT INTO `course_subscription` (`id`,`course_id`,`user_id`) VALUES 
- (1,7,7);
+ (1,7,8),
+ (2,6,8);
 /*!40000 ALTER TABLE `course_subscription` ENABLE KEYS */;
 
 
@@ -103,7 +89,7 @@ CREATE TABLE `course_unit_lesson_r` (
 INSERT INTO `course_unit_lesson_r` (`id`,`course_id`,`unit_id`,`lesson_id`) VALUES 
  (1,7,2,1),
  (2,7,2,2),
- (3,7,3,3);
+ (3,6,3,3);
 /*!40000 ALTER TABLE `course_unit_lesson_r` ENABLE KEYS */;
 
 
@@ -121,6 +107,9 @@ CREATE TABLE `courses` (
   `validTo` int(10) DEFAULT NULL,
   `duration` int(10) unsigned DEFAULT NULL,
   `color_code` varchar(45) DEFAULT NULL,
+  `subscriptionFee` varchar(45) DEFAULT NULL,
+  `brochure` varchar(255) DEFAULT NULL,
+  `categoryId` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
@@ -129,11 +118,11 @@ CREATE TABLE `courses` (
 --
 
 /*!40000 ALTER TABLE `courses` DISABLE KEYS */;
-INSERT INTO `courses` (`id`,`name`,`description`,`demo`,`validFrom`,`validTo`,`duration`,`color_code`) VALUES 
- (2,'Current Affairs','National and internation news',NULL,123123,NULL,10,'#D9EDF7'),
- (5,'Gk','GK',NULL,NULL,123123,20,'#F2DEDE'),
- (6,'History','This course will be taken by Tariq Anwar. It will be 24 hour course covering Ancient India, Modern India, World History and India Since Independence',NULL,NULL,NULL,10,'#20898C'),
- (7,'Civil','Civil',NULL,NULL,NULL,30,'#F092B0');
+INSERT INTO `courses` (`id`,`name`,`description`,`demo`,`validFrom`,`validTo`,`duration`,`color_code`,`subscriptionFee`,`brochure`,`categoryId`) VALUES 
+ (2,'Current Affairs','National and internation news',NULL,123123,NULL,10,'#D9EDF7','100',NULL,1),
+ (5,'Gk','GK',NULL,NULL,123123,20,'#F2DEDE','200',NULL,2),
+ (6,'History','This course will be taken by Tariq Anwar. It will be 24 hour course covering Ancient India, Modern India, World History and India Since Independence',NULL,NULL,NULL,10,'#20898C','150',NULL,0),
+ (7,'Civil','Civil',NULL,NULL,NULL,30,'#F092B0','300',NULL,3);
 /*!40000 ALTER TABLE `courses` ENABLE KEYS */;
 
 
@@ -155,36 +144,47 @@ CREATE TABLE `courses_instructor` (
 
 /*!40000 ALTER TABLE `courses_instructor` DISABLE KEYS */;
 INSERT INTO `courses_instructor` (`id`,`course_id`,`user_id`) VALUES 
- (5,5,7),
+ (5,5,8),
  (6,5,9),
- (7,6,7),
+ (7,6,8),
  (8,6,2),
- (13,2,3),
+ (13,2,8),
  (14,2,7),
- (15,7,3),
+ (15,7,8),
  (16,7,4);
 /*!40000 ALTER TABLE `courses_instructor` ENABLE KEYS */;
 
 
 --
--- Definition of table `lesson_commints`
+-- Definition of table `lesson_comments`
 --
 
-DROP TABLE IF EXISTS `lesson_commints`;
-CREATE TABLE `lesson_commints` (
+DROP TABLE IF EXISTS `lesson_comments`;
+CREATE TABLE `lesson_comments` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
   `lesson_id` int(10) NOT NULL,
-  `commints` varchar(100) NOT NULL,
+  `comments` varchar(100) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `lesson_commints`
+-- Dumping data for table `lesson_comments`
 --
 
-/*!40000 ALTER TABLE `lesson_commints` DISABLE KEYS */;
-/*!40000 ALTER TABLE `lesson_commints` ENABLE KEYS */;
+/*!40000 ALTER TABLE `lesson_comments` DISABLE KEYS */;
+INSERT INTO `lesson_comments` (`id`,`lesson_id`,`comments`,`timestamp`,`user_id`) VALUES 
+ (1,1,'qwer tyu iop','2016-06-29 17:05:41',8),
+ (2,1,'asasd adasdsad asdsadsa asdasdsad','2016-06-29 17:05:41',9),
+ (3,1,'fxvtfbdfbf fgb vfbgcvb gf','2016-06-29 17:05:41',8),
+ (4,1,'qwere dgh hjkmj','2016-06-29 17:47:11',9),
+ (5,1,'This is good','2016-06-29 17:57:19',8),
+ (6,1,'Thats good','2016-06-29 18:59:13',8),
+ (8,2,'sgfsdgd','2016-06-30 01:07:00',8),
+ (9,1,'Just like that','2016-06-30 13:12:44',8),
+ (10,1,'just for fun','2016-06-30 13:18:17',8);
+/*!40000 ALTER TABLE `lesson_comments` ENABLE KEYS */;
 
 
 --
@@ -196,14 +196,19 @@ CREATE TABLE `lesson_files` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
   `lesson_id` int(10) NOT NULL,
   `file` varchar(100) NOT NULL,
+  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lesson_files`
 --
 
 /*!40000 ALTER TABLE `lesson_files` DISABLE KEYS */;
+INSERT INTO `lesson_files` (`id`,`lesson_id`,`file`,`name`) VALUES 
+ (1,1,'text.txt',''),
+ (2,1,'pdffile.pdf',''),
+ (3,3,'text1.pdf','');
 /*!40000 ALTER TABLE `lesson_files` ENABLE KEYS */;
 
 
@@ -258,59 +263,35 @@ INSERT INTO `units` (`id`,`name`,`description`) VALUES
 
 
 --
--- Definition of table `user_info`
+-- Definition of table `user`
 --
 
-DROP TABLE IF EXISTS `user_info`;
-CREATE TABLE `user_info` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
   `id` int(70) NOT NULL AUTO_INCREMENT,
-  `userId` int(70) NOT NULL,
-  `firstName` varchar(45) NOT NULL,
-  `lastName` varchar(45) DEFAULT NULL,
+  `fullName` varchar(45) DEFAULT NULL,
   `joinDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `about` varchar(100) DEFAULT NULL,
-  `profilePhoto` varchar(45) DEFAULT NULL,
+  `profilePhoto` varchar(200) DEFAULT NULL,
   `billingAddress` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user_info`
---
-
-/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
-INSERT INTO `user_info` (`id`,`userId`,`firstName`,`lastName`,`joinDate`,`about`,`profilePhoto`,`billingAddress`) VALUES 
- (6,7,'Udit','Negi','2016-06-25 16:23:51','I am fine.',NULL,NULL),
- (7,9,'admin','admin','2016-06-26 19:37:29',NULL,NULL,NULL);
-/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
-
-
---
--- Definition of table `user_login`
---
-
-DROP TABLE IF EXISTS `user_login`;
-CREATE TABLE `user_login` (
-  `id` int(70) NOT NULL AUTO_INCREMENT,
   `email` varchar(45) NOT NULL,
-  `phone` varchar(10) NOT NULL,
-  `password` varchar(100) DEFAULT NULL,
+  `phone` varchar(45) NOT NULL,
   `status` varchar(45) DEFAULT NULL,
   `profileType` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `user_email_UNIQUE` (`email`) USING BTREE,
-  UNIQUE KEY `user_phone_UNIQUE` (`phone`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user_login`
+-- Dumping data for table `user`
 --
 
-/*!40000 ALTER TABLE `user_login` DISABLE KEYS */;
-INSERT INTO `user_login` (`id`,`email`,`phone`,`password`,`status`,`profileType`) VALUES 
- (7,'negi.udit@gmail.com','9900015910','81dc9bdb52d04dc20036dbd8313ed055',NULL,'student'),
- (9,'admin@gmail.com','1234567890','81dc9bdb52d04dc20036dbd8313ed055',NULL,'admin');
-/*!40000 ALTER TABLE `user_login` ENABLE KEYS */;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`,`fullName`,`joinDate`,`about`,`profilePhoto`,`billingAddress`,`email`,`phone`,`status`,`profileType`,`password`) VALUES 
+ (8,'Udit Negi','2016-06-28 19:50:36','just fine',NULL,NULL,'negi.udit@gmail.com','9900015910','active','student','81dc9bdb52d04dc20036dbd8313ed055'),
+ (9,'Admin','2016-06-28 19:51:13','fine','\\imagesPath\\vEtkDZofWpdbp6lIOOV-mTFN.png','null','admin@gmail.com','1234567890','null','admin','81dc9bdb52d04dc20036dbd8313ed055'),
+ (10,'Ayush Sinha','2016-07-01 15:33:18',NULL,NULL,NULL,'ayush.sinha@gmail.com','9874563210','active','instructor','81dc9bdb52d04dc20036dbd8313ed055');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 
 
