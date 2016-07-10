4c3fb1094151a7cd48d0d21391d38392496f2e30
rohan <negi.rohan@gmail.com>
2016-07-10 13:02:40 +0530

    mojo purpose change

--- videoDb.sql
+++ videoDb.sql
@@ -33,21 +33,6 @@ CREATE TABLE `categories` (
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `categories`
---
-
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES  (1,'Categoyr1'),
- (2,'Category2'),
- (3,'Category3'),
- (4,'Category4'),
- (5,'Category 5'),
- (6,'Category 6'),
- (7,'Category 7');
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-
-
---
 -- Definition of table `course_subscription`
 --
 
@@ -58,16 +43,6 @@ CREATE TABLE `course_subscription` (
   `userId` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `course_subscription`
---
-
-/*!40000 ALTER TABLE `course_subscription` DISABLE KEYS */;
-INSERT INTO `course_subscription` VALUES  (1,7,8),
- (2,6,8);
-/*!40000 ALTER TABLE `course_subscription` ENABLE KEYS */;
-
 
 --
 -- Definition of table `course_unit_lesson_r`
@@ -81,18 +56,6 @@ CREATE TABLE `course_unit_lesson_r` (
   `lessonId` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `course_unit_lesson_r`
---
-
-/*!40000 ALTER TABLE `course_unit_lesson_r` DISABLE KEYS */;
-INSERT INTO `course_unit_lesson_r` VALUES  (1,7,2,1),
- (2,7,2,2),
- (3,6,3,3),
- (4,11,28,4);
-/*!40000 ALTER TABLE `course_unit_lesson_r` ENABLE KEYS */;
-
 
 --
 -- Definition of table `courses`
@@ -118,19 +81,6 @@ CREATE TABLE `courses` (
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `courses`
---
-
-/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
-INSERT INTO `courses` VALUES  (2,'Current Affairs','National and internation news','http://d38mgtvgmsvii9.cloudfront.net/rising/2MayCurrAff/2MayCurrAff.m3u8',123123,NULL,10,'#D9EDF7','100','null',1,'null',0,'https://s3-ap-southeast-1.amazonaws.com/flavido-encodes/rising/2nd+May.jpg'),
- (5,'Gk','GK','http://d38mgtvgmsvii9.cloudfront.net/rising/3MayCurrAff/3MayCurrAff.m3u8',NULL,123123,20,'#F2DEDE','200','http://localhost:3000\\filesPath\\rOb6Kf16RmCbiCCYGatM15HF.pdf',2,'Node.js Recipes - Cory Gackenheimer.pdf',0,'https://s3-ap-southeast-1.amazonaws.com/flavido-encodes/rising/3rd+May.jpg'),
- (6,'History','This course will be taken by Tariq Anwar. It will be 24 hour course covering Ancient India, Modern India, World History and India Since Independence','http://d38mgtvgmsvii9.cloudfront.net/rising/4JulyRisingCurrAff-Rev1/4JulyRisingCurrAff-Rev1.m3u8',NULL,NULL,10,'#20898C','150','null',0,'null',0,'http://i.imgur.com/t1nI8bJ.jpg'),
- (7,'Civil','Civil','//www.youtube.com/v/ylLzyHk54Z0',NULL,NULL,30,'#F092B0','300',NULL,3,NULL,0,NULL),
- (11,'Economics','Economics world wide','',NULL,NULL,NULL,'#B2D3BA',NULL,'http://localhost:3000\\filesPath\\fA-TGxr-cCGIfMw8gHUcgTvF.pdf',3,'Node.js Recipes - Cory Gackenheimer.pdf',0,NULL);
-/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
-
-
---
 -- Definition of table `courses_instructor`
 --
 
@@ -143,22 +93,6 @@ CREATE TABLE `courses_instructor` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_index` (`userId`,`courseId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `courses_instructor`
---
-
-/*!40000 ALTER TABLE `courses_instructor` DISABLE KEYS */;
-INSERT INTO `courses_instructor` VALUES  (13,2,8,0),
- (14,2,7,0),
- (39,7,8,0),
- (40,8,10,0),
- (41,9,10,0),
- (42,10,10,0),
- (49,11,10,0),
- (65,5,10,0);
-/*!40000 ALTER TABLE `courses_instructor` ENABLE KEYS */;
-
 
 --
 -- Definition of table `lesson_comments`
@@ -176,24 +110,6 @@ CREATE TABLE `lesson_comments` (
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `lesson_comments`
---
-
-/*!40000 ALTER TABLE `lesson_comments` DISABLE KEYS */;
-INSERT INTO `lesson_comments` VALUES  (1,1,'qwer tyu iop','2016-06-29 17:05:41',8,0),
- (2,1,'asasd adasdsad asdsadsa asdasdsad','2016-06-29 17:05:41',9,0),
- (3,1,'fxvtfbdfbf fgb vfbgcvb gf','2016-06-29 17:05:41',8,0),
- (4,1,'qwere dgh hjkmj','2016-06-29 17:47:11',9,0),
- (5,1,'This is good','2016-06-29 17:57:19',8,0),
- (6,1,'Thats good','2016-06-29 18:59:13',8,0),
- (8,2,'sgfsdgd','2016-06-30 01:07:00',8,0),
- (9,1,'Just like that','2016-06-30 13:12:44',8,0),
- (10,1,'just for fun','2016-06-30 13:18:17',8,0),
- (11,1,'its fun','2016-07-05 14:53:42',9,0);
-/*!40000 ALTER TABLE `lesson_comments` ENABLE KEYS */;
-
-
---
 -- Definition of table `lesson_files`
 --
 
@@ -205,19 +121,6 @@ CREATE TABLE `lesson_files` (
   `fileName` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `lesson_files`
---
-
-/*!40000 ALTER TABLE `lesson_files` DISABLE KEYS */;
-INSERT INTO `lesson_files` VALUES  (1,1,'text.txt','text.txt'),
- (2,1,'pdffile.pdf','pdffile.pdf'),
- (3,3,'text1.pdf','text1.pdf'),
- (4,2,'http://localhost:3000\\filesPath\\2Xw-5z8rIPi1XXnHDkNyFgM9.pdf','Node.js Design Patterns - Casciaro, Mario [PDF][StormRG].pdf'),
- (5,2,'http://localhost:3000\\filesPath\\SfLpxqqD-JLNAAcvnM0DJ_6x.pdf','Node.js Recipes - Cory Gackenheimer.pdf');
-/*!40000 ALTER TABLE `lesson_files` ENABLE KEYS */;
-
 
 --
 -- Definition of table `lessons`
@@ -235,18 +138,6 @@ CREATE TABLE `lessons` (
   `poster` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `lessons`
---
-
-/*!40000 ALTER TABLE `lessons` DISABLE KEYS */;
-INSERT INTO `lessons` VALUES  (1,'lesson1','this is the lesson 1','http://d38mgtvgmsvii9.cloudfront.net/rising/4JulyRisingCurrAff-Rev1/4JulyRisingCurrAff-Rev1.m3u8',NULL,10,0,'http://i.imgur.com/t1nI8bJ.jpg'),
- (2,'lesson2','This is the lesson 2','//www.youtube.com/v/ylLzyHk54Z0',NULL,10,0,'https://s3-ap-southeast-1.amazonaws.com/flavido-encodes/rising/2nd+May.jpg'),
- (3,'lesson3','This is the lesson 3',NULL,NULL,10,0,NULL),
- (4,'Lesson4','Lesson number 4','Video link',NULL,14,0,NULL);
-/*!40000 ALTER TABLE `lessons` ENABLE KEYS */;
-
 
 --
 -- Definition of table `payment_details`
@@ -267,18 +158,6 @@ CREATE TABLE `payment_details` (
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `payment_details`
---
-
-/*!40000 ALTER TABLE `payment_details` DISABLE KEYS */;
-INSERT INTO `payment_details` VALUES  (7,'fcae0d9e73c44b1d9af333b2ae01f380','9900015910','Subscription: Udit Negi for Gk','200','negi.udit@gmail.com','Udit Negi','8','5'),
- (8,'365ff08b333b47178bc7936aab0ebf7b','9900015910','Subscription: Udit Negi for Gk','200','negi.udit@gmail.com','Udit Negi','8','5'),
- (9,'06e6a62653bc4471bb88e59e2833ff5e','9900015910','Subscription: Udit Negi for Gk','200','negi.udit@gmail.com','Udit Negi','8','5'),
- (10,'d846952c8c9142e0bca812feeab40528','9900015910','Subscription: Udit Negi for Gk','200','negi.udit@gmail.com','Udit Negi','8','5');
-/*!40000 ALTER TABLE `payment_details` ENABLE KEYS */;
-
-
---
 -- Definition of table `payment_log`
 --
 
@@ -294,20 +173,6 @@ CREATE TABLE `payment_log` (
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `payment_log`
---
-
-/*!40000 ALTER TABLE `payment_log` DISABLE KEYS */;
-INSERT INTO `payment_log` VALUES  (6,'fcae0d9e73c44b1d9af333b2ae01f380',NULL,NULL,NULL,'Pending'),
- (8,'8b6080c3e97149d684f2d7cf05792313','MOJO5369558911814992','3.80','800b13088cb9704fe55d8cec77ef9615097526f1','Credit'),
- (9,'b0da0313f418443a9b01d7c9cf3b957c','MOJO2391764479175223','3.80','0d3f1e7920f973160b572a80279cb73cc2ed9ddb','Credit'),
- (10,'365ff08b333b47178bc7936aab0ebf7b',NULL,NULL,NULL,'Pending'),
- (11,'06e6a62653bc4471bb88e59e2833ff5e',NULL,NULL,NULL,'Pending'),
- (12,'d846952c8c9142e0bca812feeab40528',NULL,NULL,NULL,'Pending');
-/*!40000 ALTER TABLE `payment_log` ENABLE KEYS */;
-
-
---
 -- Definition of table `units`
 --
 
@@ -320,21 +185,6 @@ CREATE TABLE `units` (
   `isDeleted` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `units`
---
-
-/*!40000 ALTER TABLE `units` DISABLE KEYS */;
-INSERT INTO `units` VALUES  (2,'Civil1','civil1',7,0),
- (3,'History1','history2',6,1),
- (11,'Civil2','Civil2',7,0),
- (12,'Civil3','Civil3',7,0),
- (18,'civil4','civil4',7,0),
- (28,'Eco1','Eco1',11,0),
- (29,'Eco2','Eco2',11,0);
-/*!40000 ALTER TABLE `units` ENABLE KEYS */;
-
 
 --
 -- Definition of table `user`
@@ -361,10 +211,8 @@ CREATE TABLE `user` (
 --
 
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES  (8,'Udit Negi','2016-06-28 19:50:36','just fine','http://localhost:3000\\imagesPath\\vllR_RcJQ5JeoJu7iTIgSVxf.png','','negi.udit@gmail.com','9900015910','active','student','81dc9bdb52d04dc20036dbd8313ed055'),
- (9,'Admin','2016-06-28 19:51:13','fine','http://localhost:3000\\imagesPath\\m4Cf1sb1rrX4DQf94tXuZ_jL.png','','admin@gmail.com','1234567890','null','admin','81dc9bdb52d04dc20036dbd8313ed055'),
- (10,'Ayush Sinha','2016-07-01 15:33:18',NULL,NULL,NULL,'ayush.sinha@gmail.com','9874563210','active','instructor','81dc9bdb52d04dc20036dbd8313ed055'),
- (11,'Ayushman','2016-07-04 20:48:11',NULL,NULL,NULL,'ayush@forumias.com','7070700521',NULL,'student','91e01cf003a151f5694d07c70e908994');
+INSERT INTO `user` VALUES
+ (1,'Admin','2016-06-28 19:51:13','fine','http://localhost:3000\\imagesPath\\m4Cf1sb1rrX4DQf94tXuZ_jL.png','','admin@gmail.com','1234567890','null','admin','81dc9bdb52d04dc20036dbd8313ed055');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 
