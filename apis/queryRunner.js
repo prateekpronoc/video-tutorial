dc6a8bddc43e608ca67dbad90e3e9c4550024ee0
rohan <negi.rohan@gmail.com>
2016-07-11 15:47:26 +0530

    Forget password implementation

--- apis/queryRunner.js
+++ apis/queryRunner.js
@@ -235,7 +235,7 @@ var self = {
                     subject: 'Email Example', // Subject line
                     text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                         'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                        'http://' + request.host + '/reset/' + token + '\n\n' +
+                        request.host + token + '\n\n' +
                         'If you did not request this, please ignore this email and your password will remain unchanged.\n'
                 };
                 transporter.sendMail(mailOptions, function(err, info) {
