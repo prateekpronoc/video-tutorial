b393261bdff1b0647f64f987362393e429e2b01f
rohan <negi.rohan@gmail.com>
2016-07-13 18:30:02 +0530

    db Changes

--- videoDb.sql
+++ videoDb.sql
@@ -27,7 +27,7 @@ USE tutorialsdb;
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(70) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
@@ -38,11 +38,11 @@ CREATE TABLE `categories` (
 
 DROP TABLE IF EXISTS `course_subscription`;
 CREATE TABLE `course_subscription` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `courseId` int(10) NOT NULL,
-  `userId` int(10) NOT NULL,
+  `id` int(70) unsigned NOT NULL AUTO_INCREMENT,
+  `courseId` int(70) unsigned NOT NULL,
+  `userId` int(70) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Definition of table `course_unit_lesson_r`
@@ -50,10 +50,10 @@ CREATE TABLE `course_subscription` (
 
 DROP TABLE IF EXISTS `course_unit_lesson_r`;
 CREATE TABLE `course_unit_lesson_r` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `courseId` int(10) NOT NULL,
-  `unitId` int(10) NOT NULL,
-  `lessonId` int(10) NOT NULL,
+  `id` int(70) unsigned NOT NULL AUTO_INCREMENT,
+  `courseId` int(70) unsigned NOT NULL,
+  `unitId` int(70) unsigned NOT NULL,
+  `lessonId` int(70) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
@@ -63,20 +63,20 @@ CREATE TABLE `course_unit_lesson_r` (
 
 DROP TABLE IF EXISTS `courses`;
 CREATE TABLE `courses` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `demoVideo` varchar(100) DEFAULT NULL,
-  `validFrom` int(10) DEFAULT NULL,
-  `validTo` int(10) DEFAULT NULL,
+  `id` int(70) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `demoVideo` varchar(255) DEFAULT NULL,
+  `validFrom` bigint(20) unsigned DEFAULT NULL,
+  `validTo` bigint(20) unsigned DEFAULT NULL,
   `duration` int(10) unsigned DEFAULT NULL,
   `color_code` varchar(45) DEFAULT NULL,
   `subscriptionFee` varchar(45) DEFAULT NULL,
   `filePath` varchar(255) DEFAULT NULL,
   `categoryId` int(10) unsigned DEFAULT NULL,
-  `fileName` varchar(45) DEFAULT NULL,
+  `fileName` varchar(100) DEFAULT NULL,
   `isDeleted` tinyint(1) DEFAULT '0',
-  `demoPoster` varchar(100) DEFAULT NULL,
+  `demoPoster` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
@@ -86,9 +86,9 @@ CREATE TABLE `courses` (
 
 DROP TABLE IF EXISTS `courses_instructor`;
 CREATE TABLE `courses_instructor` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `courseId` int(10) NOT NULL,
-  `userId` int(10) NOT NULL,
+  `id` int(70) unsigned NOT NULL AUTO_INCREMENT,
+  `courseId` int(70) unsigned NOT NULL,
+  `userId` int(70) unsigned NOT NULL,
   `isDeleted` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_index` (`userId`,`courseId`)
@@ -100,9 +100,9 @@ CREATE TABLE `courses_instructor` (
 
 DROP TABLE IF EXISTS `lesson_comments`;
 CREATE TABLE `lesson_comments` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `lessonId` int(10) NOT NULL,
-  `comments` varchar(100) NOT NULL,
+  `id` int(70) unsigned NOT NULL AUTO_INCREMENT,
+  `lessonId` int(70) unsigned NOT NULL,
+  `comments` varchar(255) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `userId` int(10) unsigned NOT NULL,
   `isDeleted` tinyint(1) DEFAULT '0',
@@ -115,10 +115,10 @@ CREATE TABLE `lesson_comments` (
 
 DROP TABLE IF EXISTS `lesson_files`;
 CREATE TABLE `lesson_files` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `lessonId` int(10) NOT NULL,
-  `filePath` varchar(100) NOT NULL,
-  `fileName` varchar(100) NOT NULL,
+  `id` int(70) unsigned NOT NULL AUTO_INCREMENT,
+  `lessonId` int(70) unsigned NOT NULL,
+  `filePath` varchar(255) NOT NULL,
+  `fileName` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
@@ -128,14 +128,14 @@ CREATE TABLE `lesson_files` (
 
 DROP TABLE IF EXISTS `lessons`;
 CREATE TABLE `lessons` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `description` varchar(100) NOT NULL,
-  `video` varchar(100) DEFAULT NULL,
-  `air_date` int(10) DEFAULT NULL,
+  `id` int(70) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `video` varchar(255) DEFAULT NULL,
+  `air_date` varchar(20) DEFAULT NULL,
   `duration` int(100) unsigned DEFAULT NULL,
   `isDeleted` tinyint(1) DEFAULT '0',
-  `poster` varchar(100) DEFAULT NULL,
+  `poster` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
@@ -145,15 +145,15 @@ CREATE TABLE `lessons` (
 
 DROP TABLE IF EXISTS `payment_details`;
 CREATE TABLE `payment_details` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `payment_request_id` varchar(200) DEFAULT NULL,
-  `phone` varchar(100) DEFAULT NULL,
-  `purpose` varchar(100) DEFAULT NULL,
+  `id` int(70) unsigned NOT NULL AUTO_INCREMENT,
+  `payment_request_id` varchar(255) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `purpose` varchar(40) DEFAULT NULL,
   `amount` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
-  `fullName` varchar(100) DEFAULT NULL,
-  `userId` varchar(100) DEFAULT NULL,
-  `courseId` varchar(100) DEFAULT NULL,
+  `fullName` varchar(255) DEFAULT NULL,
+  `userId` int(70) unsigned DEFAULT NULL,
+  `courseId` int(70) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
@@ -163,11 +163,11 @@ CREATE TABLE `payment_details` (
 
 DROP TABLE IF EXISTS `payment_log`;
 CREATE TABLE `payment_log` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `payment_request_id` varchar(200) DEFAULT NULL,
-  `payment_id` varchar(200) DEFAULT NULL,
+  `id` int(70) unsigned NOT NULL AUTO_INCREMENT,
+  `payment_request_id` varchar(255) DEFAULT NULL,
+  `payment_id` varchar(255) DEFAULT NULL,
   `fees` varchar(100) DEFAULT NULL,
-  `mac` varchar(100) DEFAULT NULL,
+  `mac` varchar(255) DEFAULT NULL,
   `status` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
@@ -178,9 +178,9 @@ CREATE TABLE `payment_log` (
 
 DROP TABLE IF EXISTS `units`;
 CREATE TABLE `units` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `description` varchar(100) NOT NULL,
+  `id` int(70) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(200) NOT NULL,
   `courseId` int(10) unsigned NOT NULL,
   `isDeleted` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
@@ -192,27 +192,27 @@ CREATE TABLE `units` (
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(70) NOT NULL AUTO_INCREMENT,
-  `fullName` varchar(45) DEFAULT NULL,
+  `id` int(70) unsigned NOT NULL,
+  `fullName` varchar(255) DEFAULT NULL,
   `joinDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `about` varchar(100) DEFAULT NULL,
-  `profilePhoto` varchar(200) DEFAULT NULL,
-  `billingAddress` varchar(100) DEFAULT NULL,
+  `about` varchar(200) DEFAULT NULL,
+  `profilePhoto` varchar(255) DEFAULT NULL,
+  `billingAddress` varchar(255) DEFAULT NULL,
   `email` varchar(45) NOT NULL,
-  `phone` varchar(45) NOT NULL,
+  `phone` varchar(20) NOT NULL,
   `status` varchar(45) DEFAULT NULL,
   `profileType` varchar(45) DEFAULT NULL,
-  `password` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES
- (1,'Admin','2016-06-28 19:51:13','fine','http://localhost:3000\\imagesPath\\m4Cf1sb1rrX4DQf94tXuZ_jL.png','','admin@gmail.com','1234567890','null','admin','81dc9bdb52d04dc20036dbd8313ed055');
+INSERT INTO `user` (`id`,`fullName`,`joinDate`,`about`,`profilePhoto`,`billingAddress`,`email`,`phone`,`status`,`profileType`,`password`) VALUES 
+ (9,'Admin','2016-06-28 19:51:13','fine','null','','admin@gmail.com','1234567890','null','admin','81dc9bdb52d04dc20036dbd8313ed055');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 
